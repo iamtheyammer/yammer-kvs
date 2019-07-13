aad73d78fb246ee5decac59ce6a0d8d2e22adc88
iamtheyammer <32084206+iamtheyammer@users.noreply.github.com>
2019-07-13 00:17:45 -0700

    Fix false values not passing validation

--- src/validate/kvp.js
+++ src/validate/kvp.js
@@ -1,6 +1,8 @@
 module.exports = (kvps) => {
   return !kvps.some((kvp) => {
-    if (!kvp.key || !kvp.value) return true;
+    if (!kvp.key || 
+      kvp.value === undefined || 
+      kvp.value === null) return true;
     if (kvp.key.match(/[A-Z0-9_]+/)[0] !== kvp.key) return true;
     return false;
   });
